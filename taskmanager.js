d60f2de287ed6dd620ac9eeb71fc62b4a8632035
optimisticsjn <114971902+optimisticsjn@users.noreply.github.com>
2024-06-11 11:18:06 +0530

    Add files via upload

--- taskmanager.js
+++ taskmanager.js
@@ -16,45 +16,61 @@ const taskmanager={
         
       completeTask(taskdone){
         
-       let check=tasks.includes(taskdone)
-        if(check===true){
-           
-            this.currentstatus=true,
+       //let check=tasks.includes(taskdone)
+       for (let check =0;check<tasks.length;check++)
+        {
+          
+          if (tasks[check]===taskdone){
+            tasks[check].currentstatus=true,
         console.log(`this ${taskdone} is done ;)`)
+           } 
         }
+
+         
       },
 
       filterTask(){
-          tasks.map(function(element){
-          
-            if(element.currentstatus==false){
-              let undonetask=element
-              console.log(`these tasks are incomplete:${undonetask}`) 
-            } })
+         setInterval(()=>{
+
+          const undonetask=tasks.filter(tasks=>{
+            return tasks.currentstatus==false}).map(t=>t.todo);
+                 console.log(`these tasks are incomplete:${undonetask}`) 
+
+         },8000)   
+      
+            } 
             
-      }
+      
          
           
     }
 
-     console.log('you can add upto 2 tasks')
+    console.log('you can add upto 2 tasks')
     for (let i=1;i<3;i++){
       let input=prompt(`add your task${i}`)
       taskmanager.addTask(input)
 
-    }
-   let id=setInterval(()=>{
-    console.log( taskmanager.listTask()) },3000)
-    setTimeout(() => {
-      clearInterval(id)
-    }, 9000);
+   }
+   // prompt(`view${}`)
+
+  // let id=setInterval(()=>{
+   // console.log( taskmanager.listTask()) },3000)
+   // setTimeout(() => {
+   //   clearInterval(id)
+   // }, 9000);
 
 
-    //taskmanager.addTask("learn")
+   // taskmanager.addTask("learn")
+   //setTimeout(()=>{console.log( taskmanager.completeTask("learn"))
+    //console.log(taskmanager.listTask())
+  //console.log(taskmanager.filterTask())
+ // },4000)
+ //let a= ['learn']
+ // console.log( a[0]=='learn')
+
     //taskmanager.addTask('write')
    // taskmanager.addTask("jog")
    // console.log(tasks)
-   // console.log(tasks.includes("jog"))
    // console.log(tasks.includes('write'))
 
 
