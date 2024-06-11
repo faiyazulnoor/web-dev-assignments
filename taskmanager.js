b1dc09b091388fa674c8a14e618a54d394448114
optimisticsjn <114971902+optimisticsjn@users.noreply.github.com>
2024-06-11 15:49:23 +0530

    Add files via upload

--- taskmanager.js
+++ taskmanager.js
@@ -9,7 +9,7 @@ const taskmanager={
      },
 
     listTask(){tasks.forEach(function(el){
-               console.log(el);
+               console.log(el) ;
                
                  }) 
             },
@@ -17,27 +17,24 @@ const taskmanager={
       completeTask(taskdone){
         
        //let check=tasks.includes(taskdone)
-       for (let check =0;check<tasks.length;check++)
-        {
-          
-          if (tasks[check]===taskdone){
-            tasks[check].currentstatus=true,
-        console.log(`this ${taskdone} is done ;)`)
-           } 
-        }
+       tasks.forEach (function(check){
 
-         
+        if (check.todo===taskdone){
+          check.currentstatus=true,
+      console.log(` ${taskdone} is done ;)`)
+              }
+          } )
+                    
       },
 
       filterTask(){
-         setInterval(()=>{
+
 
           const undonetask=tasks.filter(tasks=>{
             return tasks.currentstatus==false}).map(t=>t.todo);
-                 console.log(`these tasks are incomplete:${undonetask}`) 
+                 console.log(`these tasks are incomplete: ${undonetask}`) 
 
-         },8000)   
-      
+              
             } 
             
       
@@ -45,36 +42,31 @@ const taskmanager={
           
     }
 
-    console.log('you can add upto 2 tasks')
-    for (let i=1;i<3;i++){
-      let input=prompt(`add your task${i}`)
+    console.log('you can add upto 3 tasks')
+    
+    for (let i=1;i<=3;i++){
+       let input=prompt(`add your task ${i}`)
       taskmanager.addTask(input)
+      //taskmanager.listTask();
+      
 
    }
-   // prompt(`view${}`)
+   setTimeout(()=>{
+      let userdid=prompt('task done')
+   taskmanager.completeTask(userdid)},8000);
 
-  // let id=setInterval(()=>{
-   // console.log( taskmanager.listTask()) },3000)
-   // setTimeout(() => {
-   //   clearInterval(id)
-   // }, 9000);
+      let id=setInterval(()=>{
+    console.log(taskmanager.filterTask())},5000)
+   setTimeout(() => {
+     clearInterval(id)
+    }, 10000);
 
+console.log(taskmanager.listTask());//not working???
 
-   // taskmanager.addTask("learn")
-   //setTimeout(()=>{console.log( taskmanager.completeTask("learn"))
-    //console.log(taskmanager.listTask())
-  //console.log(taskmanager.filterTask())
- // },4000)
- //let a= ['learn']
- // console.log( a[0]=='learn')
-
-    //taskmanager.addTask('write')
-   // taskmanager.addTask("jog")
-   // console.log(tasks)
-   // console.log(tasks.includes('write'))
+   
 
 
-    //console.log(Object.entries(tasks))
+    
    
 
 //task1-------------------------
